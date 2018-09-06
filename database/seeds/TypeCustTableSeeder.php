650258bdeae9c20527b247d0e6d64d2cf925db2f
Made Adi Wiradarma <adi.wiradarma423@gmail.com>
2018-09-06 17:31:39 +0700

    Seeding

---
+++ database/seeds/TypeCustTableSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypeCustTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('type_custs')->insert([
+            'name' => 'Facebook',
+            'type_input' => 'Out-Site',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Ms Rumah',
+            'type_input' => 'Out-Site',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Demo',
+            'type_input' => 'Out-Site',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'CFD',
+            'type_input' => 'Out-Site',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'MGM',
+            'type_input' => 'Out-Site',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Facebook',
+            'type_input' => 'Undangan',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Undangan HUT',
+            'type_input' => 'Undangan',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Undangan Bank',
+            'type_input' => 'Undangan',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Telkomsel',
+            'type_input' => 'Undangan',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'MGM',
+            'type_input' => 'Undangan',
+        ]);
+
+        DB::table('type_custs')->insert([
+            'name' => 'Whatsapp',
+            'type_input' => 'Undangan',
+        ]);
+    }
+}
