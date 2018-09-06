e7775dd99ac98d5cfb7c9a445410ab44326167d7
Albertus Tjahjono <fleksaboy@gmail.com>
2018-09-06 17:45:40 +0700

    Added Controller Branch 2

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
+use App\Branch;
+use Auth;
+use DB;
 
 class BranchController extends Controller
 {
@@ -11,9 +16,41 @@ class BranchController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = Auth::user();
+
+        if($user->can('all-country-branch'))
+        {
+            $branches = Branch::when($request->keyword, function ($query) use ($request) {
+                $query->where('code', 'like', "%{$request->keyword}%")
+                    ->where('active', true)
+                    ->orWhere('name', 'like', "%{$request->keyword}%")
+                    ->where('active', true)
+                    ->orWhere('country', 'like', "%{$request->keyword}%");
+                    ->where('active', true)
+            })->where('active', true)->paginate(10);
+
+            $branches->appends($request->only('keyword'));
+        }
+        else
+        {
+            $branches = Branch::when($request->keyword, function ($query) use ($request) {
+                $query->where('code', 'like', "%{$request->keyword}%")
+                    ->where('active', true)
+                    ->orWhere('name', 'like', "%{$request->keyword}%")
+                    ->where('active', true)
+                    ->orWhere('country', 'like', "%{$request->keyword}%");
+                    ->where('active', true)
+            })->where([
+                ['branches.active', true],
+                ['country', $user->branch['country']]
+            ])
+            ->paginate(10);
+
+            $branches->appends($request->only('keyword'));
+        }
+        return view('branches.index', compact('branches'));
     }
 
     /**
@@ -34,18 +71,49 @@ class BranchController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //masukin dalam tampunngan validator dan di cek ada yg salah dengan RULE nya
+        $validator = \Validator::make($request->all(), [
+            'code' => 'required|unique:branches',
+            'name' => 'required|string|max:255',
+            'country' => 'required',
+        ]);
+
+        //di cek validasi nya kalo salah masuk IF, kalo berhasil masuk ELSE
+        if ($validator->fails())
+        {
+            $arr_Errors = $validator->errors()->all();
+            $arr_Keys = $validator->errors()->keys();
+            $arr_Hasil = [];
+            for ($i=0; $i < count($arr_Keys); $i++) { 
+                $arr_Hasil[$arr_Keys[$i]] = $arr_Errors[$i];
+            }
+            return response()->json(['errors'=>$arr_Hasil]);
+        }
+        else {
+            $data = $request->only('name');
+            $data['name'] = strtoupper($data['name']);
+            $data['code'] = strtoupper($request->get('code'));
+            $data['country'] = strtoupper($request->get('country'));
+
+            $id = DB::table('branches')->insertGetId([
+                'code' => $data['code'], 
+                'name' => $data['name'],
+                'country' => $data['country']
+            ]);
+
+            return response()->json(['success'=>'Berhasil !!']);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function find(Request $request)
     {
-        //
+        $branches = Branch::
     }
 
     /**
@@ -66,9 +134,37 @@ class BranchController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        //masukin dalam tampunngan validator dan di cek ada yg salah dengan RULE nya
+        $validator = \Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'country' => 'required',
+        ]);
+
+        //di cek validasi nya kalo salah masuk IF, kalo berhasil masuk ELSE
+        if ($validator->fails())
+        {
+            $arr_Errors = $validator->errors()->all();
+            $arr_Keys = $validator->errors()->keys();
+            $arr_Hasil = [];
+            for ($i=0; $i < count($arr_Keys); $i++) { 
+                $arr_Hasil[$arr_Keys[$i]] = $arr_Errors[$i];
+            }
+            return response()->json(['errors'=>$arr_Hasil]);
+        }
+        else {
+            $data = $request->only('name');
+            $data['name'] = strtoupper($data['name']);
+            $data['country'] = strtoupper($request->get('country'));
+
+            DB::table('branches')
+                ->where('id', $request->get('id'))
+                ->update(['name' => $data['name'],
+                        'country' => $data['country']]);
+
+            return response()->json(['success'=>'Berhasil !!']);
+        }
     }
 
     /**
@@ -80,5 +176,12 @@ class BranchController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function delete(Cso $cso)
+    {
+        $cso->active = false;
+        $cso->save();
+        return redirect()->route('list_csos');
     }
 }
