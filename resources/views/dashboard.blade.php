0794dcd530401c58780f84c1ff8efbe25c08b204
frezznezz <jatmikogunawan@gmail.com>
2018-10-11 18:03:53 +0700

    add dashboard edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.template')
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/chart/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
 @endsection
 @section('navmenu')
     @if(Gate::check('dashboard'))
@@ -34,14 +35,7 @@
 @section('content')
 <div class="container">
     <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-        <div class="row">
-            <ol class="breadcrumb">
-                <li><a href="#">
-                    <em class="fa fa-home"></em>
-                </a></li>
-                <li class="active">Dashboard</li>
-            </ol>
-        </div><!--/.row-->
+        
         
         <div class="row">
             <div class="col-lg-12">
@@ -53,33 +47,33 @@
             <div class="row">
                 <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
                     <div class="panel panel-teal panel-widget border-right">
-                        <div class="row no-padding"><em class="fa fa-xl fa-shopping-cart color-blue"></em>
+                        <div class="row no-padding"><em class="fa fa-xl fa-users color-blue"></em>
                             <div class="large">120</div>
-                            <div class="text-muted">New Orders</div>
+                            <div class="text-muted">Data Undangan</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
                     <div class="panel panel-blue panel-widget border-right">
-                        <div class="row no-padding"><em class="fa fa-xl fa-comments color-orange"></em>
+                        <div class="row no-padding"><em class="fa fa-xl fa-arrow-up color-orange"></em>
                             <div class="large">52</div>
-                            <div class="text-muted">Comments</div>
+                            <div class="text-muted">Data Outside</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
                     <div class="panel panel-orange panel-widget border-right">
-                        <div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
+                        <div class="row no-padding"><em class="fa fa-xl fa-arrow-down color-teal"></em>
                             <div class="large">24</div>
-                            <div class="text-muted">New Users</div>
+                            <div class="text-muted">Data Therapy</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
                     <div class="panel panel-red panel-widget ">
-                        <div class="row no-padding"><em class="fa fa-xl fa-search color-red"></em>
+                        <div class="row no-padding"><em class="fa fa-xl fa-credit-card color-red"></em>
                             <div class="large">25.2k</div>
-                            <div class="text-muted">Page Views</div>
+                            <div class="text-muted">M.P.C.</div>
                         </div>
                     </div>
                 </div>
@@ -105,7 +99,7 @@
             <div class="col-xs-6 col-md-3">
                 <div class="panel panel-default">
                     <div class="panel-body easypiechart-panel">
-                        <h4>New Orders</h4>
+                        <h4>Data Undangan</h4>
                         <div class="easypiechart" id="easypiechart-blue" data-percent="92" ><span class="percent">92%</span></div>
                     </div>
                 </div>
@@ -113,7 +107,7 @@
             <div class="col-xs-6 col-md-3">
                 <div class="panel panel-default">
                     <div class="panel-body easypiechart-panel">
-                        <h4>Comments</h4>
+                        <h4>Data Outsite</h4>
                         <div class="easypiechart" id="easypiechart-orange" data-percent="10" ><span class="percent">10%</span></div>
                     </div>
                 </div>
@@ -121,7 +115,7 @@
             <div class="col-xs-6 col-md-3">
                 <div class="panel panel-default">
                     <div class="panel-body easypiechart-panel">
-                        <h4>New Users</h4>
+                        <h4>Data Therapy</h4>
                         <div class="easypiechart" id="easypiechart-teal" data-percent="56" ><span class="percent">56%</span></div>
                     </div>
                 </div>
@@ -129,7 +123,7 @@
             <div class="col-xs-6 col-md-3">
                 <div class="panel panel-default">
                     <div class="panel-body easypiechart-panel">
-                        <h4>Visitors</h4>
+                        <h4>M.P.C.</h4>
                         <div class="easypiechart" id="easypiechart-red" data-percent="27" ><span class="percent">27%</span></div>
                     </div>
                 </div>
