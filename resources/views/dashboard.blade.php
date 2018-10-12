73e40140f3d6e37cbc2d35495016ef823b804a14
frezznezz <jatmikogunawan@gmail.com>
2018-10-12 12:39:45 +0700

    edit h1 dashboard styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,7 +38,7 @@
         
         
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-xs-12 .col-sm-12 col-md-12 col-lg-12">
                 <h1 class="page-header">Dashboard</h1>
             </div>
         </div><!--/.row-->
