eff1f4c04181f4e8c6b0af549429c47a1142a4b9
frezznezz <jatmikogunawan@gmail.com>
2018-10-26 19:17:17 +0700

    edit tambah table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -137,11 +137,58 @@
 <script>
     window.onload = function () {
         var chart1 = document.getElementById("line-chart").getContext("2d");
+        var lineChartData = {
+        labels : ["Jabary","Febuary","Mbch","April","May","June","July"],
+        datasets : [
+            {
+                label: "My First dataset",
+                fillColor : "rgba(48, 164, 255, 0)",
+                strokeColor : "rgba(48, 164, 255, 1)",
+                pointColor : "rgba(48, 164, 255, 1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : [5000,4000,5000,4000,5000,4000,5000]
+            },
+            {
+                label: "My Second dataset",
+                fillColor : "rgba(255, 181, 62, 0)",
+                strokeColor : "rgba(255, 181, 62, 1)",
+                pointColor : "rgba(255, 181, 62, 1)",
+                pointStrokeColor : "#fff",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(48, 164, 255, 1)",
+                data : [4000,5000,4000,5000,4000,5000,4000]
+            },
+            {
+                label: "My Third dataset",
+                fillColor : "rgba(30,191,174,0)",           
+                strokeColor : "rgba(30,191,174,1)",
+                pointColor : "rgba(30,191,174,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(48, 164, 255, 1)",
+                data : [3000,3000,3000,3000,3000,3000,3000]
+            },
+            {
+                label: "My Fourth dataset",
+                fillColor : "rgba(239, 64, 64, 0)",
+                strokeColor : "rgba(239, 64, 64, 1",
+                pointColor : "rgba(239, 64, 64, 1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(48, 164, 255, 1)",
+                data : [2000,2000,2000,2000,2000,2000,2000]
+            }
+        ]
+
+    }
         window.myLine = new Chart(chart1).Line(lineChartData, {
             responsive: true,
             scaleLineColor: "rgba(0,0,0,.2)",
             scaleGridLineColor: "rgba(0,0,0,.05)",
-            scaleFontColor: "#c5c7cc"
+            scaleFontColor: "#c5c7cc",
         });
     };
 </script>
